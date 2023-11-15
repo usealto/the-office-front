5f56ab7d1c6e1160984c6b32409e0b9bb1452ea8
Guillim <guigloo@msn.com>
2023-11-15 11:04:24 +0100

    removing unused properties

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,6 @@ export const environment = {
   trainxapiURL: 'https://api.usealto.com',
   auth0Domain: 'https://usealto.eu.auth0.com',
   auth0ClientId: 'gtvfSAQkXeR1UzSLfIbgVNwttvTPs16d',
-  airtableURL: 'https://airtable.com/shr0l3s2lxxXhvgao?prefill_Requested+by=',
   audience : 'https://api.usealto.com',
   trainxURL: 'https://app.usealto.com',
   theofficeURL: 'https://theoffice-api.usealto.com',
