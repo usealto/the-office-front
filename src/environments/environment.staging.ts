bbe3de9320b7572079ba867a2fb15c81e0289fc5
Guillim <guigloo@msn.com>
2023-11-17 10:10:04 +0100

    fixing staging env

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  trainxapiURL: 'https://api-staging.osc-fr1.scalingo.io',
-  auth0Domain: 'staging-alto.eu.auth0.com',
-  auth0ClientId: 'HHKbREpvuaqfsSPZwZ0q6WDeY9Pyn0oN',
+  trainxapiURL: 'https://api-test-develop.usealto.com',
+  auth0Domain: 'dev-bmttww5s.eu.auth0.com',
+  auth0ClientId: 'ThcIBQZrRso5QaZq67kCU5eFYTfZwTSK',
   audience: 'https://api.usealto.com',
-  trainxURL: 'https://trainx-staging.usealto.com',
+  trainxURL: 'https://app-develop.usealto.com',
   theofficeURL: 'https://theoffice-api-develop.usealto.com',
 };
