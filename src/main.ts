e59c4684b88849ad1ae03d0cd6e8dd2050fa75ba
Jérémy Moyson <moyson.jeremy@gmail.com>
2024-01-12 16:18:30 +0100

    Setup sentry

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,29 @@
 /// <reference types="@angular/localize" />
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import * as Sentry from '@sentry/angular-ivy';
 
 import { AppModule } from './app/app.module';
 
+Sentry.init({
+  dsn: 'https://4c5db3174b7e575494d145068d5ecdda@o4506417447174144.ingest.sentry.io/4506558940971008',
+  integrations: [
+    new Sentry.BrowserTracing({
+      // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+      tracePropagationTargets: ['localhost', /^https:\/\/getcokpit\.io/],
+    }),
+    new Sentry.Replay({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Session Replay
+  replaysSessionSampleRate: 1.0, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
