c3e3ada1cefec62f420f360331ecbb318d2d8449
Guillim <guillim@users.noreply.github.com>
2024-03-06 10:22:07 +0100

    Add "disconnect" button following jeremy feedback

--- src/app/modules/unknown-error/unknown-error.component.ts
+++ src/app/modules/unknown-error/unknown-error.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { I18ns } from '../../core/utils/i18n/I18n';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'alto-unknown-error',
@@ -7,10 +9,20 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./unknown-error.component.scss'],
 })
 export class UnknownErrorComponent implements OnInit {
-  constructor(public route: ActivatedRoute) {}
+  constructor(
+    public route: ActivatedRoute,
+    public auth: AuthService) {}
   errorMessage: string = '';
+  I18ns = I18ns
 
   ngOnInit() {
     this.errorMessage = this.route.snapshot.paramMap.get('error') || '';
+  }
+
+  logOut() {
+    console.log('heere');
+    
+    this.auth.logout({ logoutParams: { returnTo: window.location.origin } });
+    return;
   }
 }
