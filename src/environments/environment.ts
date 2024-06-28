a1862514ae93c6027c01a0edbd787a130361f735
Jérémy Moyson <moyson.jeremy@gmail.com>
2024-06-28 10:09:16 +0200

    Update environement

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,12 +4,11 @@
 
 export const environment = {
   production: false,
-  trainxapiURL: 'https://trainx-api-develop.getcockpit.io',
+  trainxapiURL: 'http://localhost:3000',
   auth0Domain: 'dev-bmttww5s.eu.auth0.com',
   auth0ClientId: 'ThcIBQZrRso5QaZq67kCU5eFYTfZwTSK',
   audience: 'https://api.usealto.com',
-  theofficeURL: 'https://theoffice-api-develop.getcockpit.io',
-  // audience: 'https://dev-bmttww5s.eu.auth0.com/api/v2',
+  theofficeURL: 'http://localhost:3001',
   trainxTheOfficeId: '5de36fd6-36c0-49ac-97bc-9e0517527d1c',
   recordxTheOfficeId: '305fd64b-f9b7-46ae-a82f-1ca36be7088a',
   stripeCustomerURL: 'https://dashboard.stripe.com/test/customers/',
